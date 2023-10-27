72af8d4c925c13b5d2cc9ae0ff67ac20d211a641
mdyousufhossain <yousafhossain3@gmail.com>
2023-10-27 22:15:52 +0600

    test/style: tailwind and globalcss has been solved its good to push for main

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -77,9 +77,10 @@ const config: Config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       }
-        
-    
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/")],
+  plugins: [
+    require("tailwindcss-animate"), require("@tailwindcss/typography")],
 }
 export default config
+
+
