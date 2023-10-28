b8310234e970da795fb1aab065b22cce8baf8ba3
mdyousufhossain <yousafhossain3@gmail.com>
2023-10-28 20:35:44 +0600

    dev:added app file structure and added eslint setupe for code style format

--- app/components/productCard.tsx
+++
@@ -1,6 +0,0 @@
-const productCard = () => {
-  return (
-    <div>productCard</div>
-  )
-}
-export default productCard
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import Image from 'next/image'
-
-export default function Home() {
+export default function Home () {
   return (
     <main>
-     <div>
-      <h1 className='h1-bold'>This is test text &apos; of bold text</h1>
-      <h2 className='h2-bold'>This is test headline 2 &apos; of bold text</h2>
-      <h3 className='h3-bold'>This is test headline 3 &apos; of bold text</h3>
-     </div>  
+      <div>
+        <h1 className="h1-bold">This is test text &apos; of bold text</h1>
+        <h2 className="h2-bold">This is test headline 2 &apos; of bold text</h2>
+        <h3 className="h3-bold">This is test headline 3 &apos; of bold text</h3>
+      </div>
     </main>
   )
 }
