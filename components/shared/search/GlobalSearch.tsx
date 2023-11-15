53ea20e889b468c61d5ff3ffdf256b9921a0bbdf
mdyousufhossain <yousafhossain3@gmail.com>
2023-11-15 22:47:49 +0600

    navbar: added new global search

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,7 +1,23 @@
+import Image from 'next/image'
+import { Input } from '@/components/ui/input'
+
 const GlobalSearch = () => {
   return (
     <div className='max-lg:hidden relative w-full max-w-[600px]'>
-        <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4"></div>
+      <div className='background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4'>
+        <Image
+          src='/assets/icons/search.svg'
+          width={24}
+          height={24}
+          className='cursor-pointer'
+        />
+        <Input
+          type='text'
+          placeholder='Search globally'
+          value=''
+          className='paragraph-reguler  placeholder text_dark400_light700 shadow-none border-none outline-none focus-visible:ring-0 focus-visible:ring-transparent focus'
+        />
+      </div>
     </div>
   )
 }
