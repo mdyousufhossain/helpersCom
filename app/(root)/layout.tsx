f6d8d0be21cc866d0e57ec6fb0cccc92ceba212f
mdyousufhossain <yousafhossain3@gmail.com>
2023-11-22 22:08:17 +0600

    added rightside bar content

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -12,7 +12,6 @@ const layout = ({ children }: { children: React.ReactNode }) => {
         <section className='max-md:pb-14 sm:px-14 flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36'>
           <div className='mx-auto w-full max-w-5xl'>{children}</div>
         </section>
-
         <RightSidebar />
       </div>
     </main>
--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 const RightSidebar = () => {
   const hotQuestions = [
@@ -6,12 +7,12 @@ const RightSidebar = () => {
     { _id: 1, title: 'test' },
     { _id: 2, title: 'test' },
     { _id: 3, title: 'test' },
-    { _id: 4, title: 'test' }
+    { _id: 4, title: 'test' },
   ]
   return (
     <section className='background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen flex-col  overflow-y-auto border-l p-6 pt-16 shadow-light-300 dark:[box-shadow:none] max-xl:hidden lg:w-[356px] '>
       <div>
-        <h3 className='h3-bold text-dark200_light900'>Top Questions</h3>
+        <h3 className='h3-bold text-dark200_light900 pt-16'>Top Questions</h3>
         <div className='mt-7 flex w-full flex-col gap-[30px]'>
           {hotQuestions.map((item) => (
             <Link
@@ -19,7 +20,14 @@ const RightSidebar = () => {
               key={item._id}
               className='flex cursor-pointer items-center justify-between gap-7'
             >
-              <p>{item.title}</p>
+              <p className='body-medium text-dark500_light700'>{item.title}</p>
+
+              <Image
+                src='/assets/icons/chevron-right.svg'
+                alt='chevron right'
+                width={20}
+                height={20}
+              />
             </Link>
           ))}
         </div>
