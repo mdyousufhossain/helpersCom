ac755aea8e0cbb1d7bd6cee4967a461535a82872
mdyousufhossain <yousafhossain3@gmail.com>
2023-11-22 11:34:14 +0600

    working on sidebar

---
+++ components/shared/LeftSidebar.tsx
@@ -0,0 +1,8 @@
+const LeftSidebar = () => {
+  return (
+    <section className='background-light900_dark200 light-border  sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:[box-shadow:none] max-sm:hidden lg:w-[266px]'>
+      leftside
+    </section>
+  )
+}
+export default LeftSidebar
