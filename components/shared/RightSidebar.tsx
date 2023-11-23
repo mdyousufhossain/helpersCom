d4105fecc06a5793999887873bee574c6e6e1c28
mdyousufhossain <yousafhossain3@gmail.com>
2023-11-23 13:39:50 +0600

    right sidebar is done : fully tested and workable some optimization is required though

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,15 +1,22 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import RenderTag from './RenderTag'
+const hotQuestions = [
+  { _id: 0, title: 'test' },
+  { _id: 1, title: 'test' },
+  { _id: 2, title: 'test' },
+  { _id: 3, title: 'test' },
+  { _id: 4, title: 'test' },
+]
 
+const popularTags = [
+  { _id: 0, name: 'javascript', totalQuestions: 5 },
+  { _id: 2, name: 'javascript', totalQuestions: 5 },
+  { _id: 3, name: 'javascript', totalQuestions: 5 },
+  { _id: 4, name: 'javascript', totalQuestions: 5 },
+  { _id: 5, name: 'javascript', totalQuestions: 5 },
+]
 const RightSidebar = () => {
-  const hotQuestions = [
-    { _id: 0, title: 'test' },
-    { _id: 1, title: 'test' },
-    { _id: 2, title: 'test' },
-    { _id: 3, title: 'test' },
-    { _id: 4, title: 'test' },
-  ]
   return (
     <section className='background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen flex-col  overflow-y-auto border-l p-6 pt-16 shadow-light-300 dark:[box-shadow:none] max-xl:hidden lg:w-[356px] '>
       <div>
@@ -34,8 +41,23 @@ const RightSidebar = () => {
           ))}
         </div>
       </div>
+
+      {/* tag */}
       <div className='mt-7 flex flex-col gap-4'>
-            <RenderTag />
+        <div>
+          <h3 className='h3-bold text-dark200_light900 '>Populer Tag</h3>
+        </div>
+        <div className='mt-7 flex flex-col gap-4'>
+          {popularTags.map((tag) => (
+            <RenderTag
+              key={tag._id}
+              _id={tag._id}
+              name={tag.name}
+              totalQuestions={tag.totalQuestions}
+              showCount
+            />
+          ))}
+        </div>
       </div>
     </section>
   )
