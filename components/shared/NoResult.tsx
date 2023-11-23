c7ac92fb286662bf0e3aa234391507cf61a79d52
mdyousufhossain <yousafhossain3@gmail.com>
2023-11-23 21:28:24 +0600

    noresult:button added and resultpage is updated again

--- components/shared/NoResult.tsx
+++ components/shared/NoResult.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from '../ui/button'
 
 const NoResult = () => {
   return (
@@ -17,12 +19,22 @@ const NoResult = () => {
         height={200}
         className='hidden object-contain dark:flex'
       />
-      <h2>No result founded or something unexpected occured 🚀</h2>
-      <p>
+      <h2 className='h2-bold text-dark200_light900 mt-8'>
+        No result founded or something unexpected occured 🚀
+      </h2>
+      <p className='body-regular text-dark500_light700 my-3.5 max-w-md text-center'>
         Be first to break the silence Ask a Questions and kickstart the
         discusstion. our query could be the next big thing others learn from.
-        Get involved
+        Get involved💡
       </p>
+      <Link href='/'>
+        <Button
+          className='paragraph-medium mt-5 min-h-[46px]
+        rounded-lg bg-primary-500 p-4 text-light-900 dark:text-light-900'
+        >
+          Ask Question
+        </Button>
+      </Link>
     </div>
   )
 }
