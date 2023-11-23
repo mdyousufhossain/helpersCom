824d88ed5774cb5999acafb1adead34ab4d9c749
mdyousufhossain <yousafhossain3@gmail.com>
2023-11-23 13:55:49 +0600

    working on home-page maybe 10% working in progress

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,21 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
 export default function Home () {
   return (
-    <main className='bg-black-500'>
-      <div>
-        <h1 className='h1-bold'>This is test text &apos; of bold text</h1>
-        <h2 className='h2-bold'>This is test headline 2 &apos; of bold text</h2>
-        <h3 className='h3-bold'>This is test headline 3 &apos; of bold text</h3>
+    <>
+      <div className='flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center'>
+        <h1 className='h1-bold text-dark100_light900'>All Questions </h1>
+        <Link href={'/ask-question'} className='flex justify-end max-sm:w-full'>
+          <Button className='primary-gradient min-h-[46px] px-4 py-3 !text-light-900'>
+            Ask a Question
+          </Button>
+        </Link>
       </div>
-    </main>
+      <div className='mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center'>
+        {/* localSearch */}
+        {/* filter */}
+      </div>
+    </>
   )
 }
