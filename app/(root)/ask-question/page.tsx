c7dc8406b0b35249b66d786a16e58ad736ff7a2f
mdyousufhossain <yousafhossain3@gmail.com>
2023-11-27 22:10:05 +0600

    fixing the form as input

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,4 +1,14 @@
-const AskQuestion = () => {
-  return <div>AskQuestion</div>
+import Questions from '@/components/forms/Questions'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='h1-bold text-dark100_light900'>Ask a Questions</h1>
+      <div className='mt-9'>
+        <Questions />
+      </div>
+    </div>
+  )
 }
-export default AskQuestion
+
+export default page
