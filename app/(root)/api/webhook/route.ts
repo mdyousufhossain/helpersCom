e94c497752fe1fadcfc7f510ea4f171a34f4ce9d
mdyousufhossain <yousafhossain3@gmail.com>
2023-12-19 20:18:31 +0600

    making route for user creation and updating

--- app/(root)/api/webhook/route.ts
+++ app/(root)/api/webhook/route.ts
@@ -2,8 +2,10 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
+import { createUser, updateUser } from '@/lib/actions/user.action'
+import { NextResponse } from 'next/server'
 
-export async function POST(req: Request) {
+export async function POST (req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
 
@@ -22,7 +24,7 @@ export async function POST(req: Request) {
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response('Error occured -- no svix headers', {
-      status: 400,
+      status: 400
     })
   }
 
@@ -40,25 +42,45 @@ export async function POST(req: Request) {
     evt = wh.verify(body, {
       'svix-id': svix_id,
       'svix-timestamp': svix_timestamp,
-      'svix-signature': svix_signature,
+      'svix-signature': svix_signature
     }) as WebhookEvent
   } catch (err) {
     console.error('Error verifying webhook:', err)
     return new Response('Error occured', {
-      status: 400,
+      status: 400
     })
   }
 
-  // Get the ID and type
-  const { id } = evt.data
   const eventType = evt.type
 
   if (eventType === 'user.created') {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data
+    const mongoUser = await createUser({
+      clerkId: id,
+      name: `${first_name} ${last_name ? `${last_name}` : ''}`,
+      username: username!,
+      email: email_addresses[0].email_address,
+      picture: image_url
+    })
 
-      
+    return NextResponse.json({ message: 'ok ', user: mongoUser })
+  } else if (eventType === 'user.updated') {
+    const { id, email_addresses, image_url, username, first_name, last_name } =
+      evt.data
+    const mongoUser = await updateUser({
+      clerkId: id,
+      updateData: {
+        name: `${first_name} ${last_name ? `${last_name}` : ''}`,
+        username: username!,
+        email: email_addresses[0].email_address,
+        picture: image_url
+      },
+      path: `/profile/${id}`
+    })
+
+    return NextResponse.json({ message: 'ok ', user: mongoUser })
   }
 
-  return new Response('', { status: 200 })
+  return new Response('', { status: 201 })
 }
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -3,7 +3,8 @@
 import { connectionToDatabase } from '../mongoose'
 
 import User from '@/database/user.question'
-import { CreateUserParams } from './shared.types'
+import { CreateUserParams, UpdateUserParams } from './shared.types'
+import { revalidatePath } from 'next/cache'
 
 // this a demo comment
 // this a seccond comment
@@ -25,12 +26,26 @@ export async function getUserById(params: any) {
 // this a demo comment
 // this a seccond comment
 
-export async function createUser (userData: CreateUserParams) {
+export async function createUser(userData: CreateUserParams) {
   try {
     connectionToDatabase()
 
     const newUser = await User.create(userData)
     return newUser
+  } catch (error) {
+    console.log(error)
+  }
+}
+export async function updateUser (params: UpdateUserParams) {
+  try {
+    connectionToDatabase()
+
+    const { clerkId, updateData, path } = params
+
+    await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true
+    })
+    revalidatePath(path)
   } catch (error) {
     console.log(error)
   }
