d3866986f797d5a3abc58c28ad0eb85100504de3
mdyousufhossain <yousafhossain3@gmail.com>
2023-12-19 20:35:07 +0600

    fully implemented the webshook

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -3,13 +3,20 @@
 import { connectionToDatabase } from '../mongoose'
 
 import User from '@/database/user.question'
-import { CreateUserParams, UpdateUserParams } from './shared.types'
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  UpdateUserParams
+} from './shared.types'
 import { revalidatePath } from 'next/cache'
+import console from 'console'
+import Question from '@/database/question.model'
+import path from 'path'
 
 // this a demo comment
 // this a seccond comment
 
-export async function getUserById(params: any) {
+export async function getUserById (params: any) {
   try {
     connectionToDatabase()
 
@@ -26,7 +33,7 @@ export async function getUserById(params: any) {
 // this a demo comment
 // this a seccond comment
 
-export async function createUser(userData: CreateUserParams) {
+export async function createUser (userData: CreateUserParams) {
   try {
     connectionToDatabase()
 
@@ -46,6 +53,42 @@ export async function updateUser (params: UpdateUserParams) {
       new: true
     })
     revalidatePath(path)
+  } catch (error) {
+    console.log(error)
+  }
+}
+export async function deleteUser (params: DeleteUserParams) {
+  try {
+    connectionToDatabase()
+
+    const { clerkId } = params
+
+    const user = await User.findOneAndDelete({ clerkId })
+
+    if (!user) {
+      throw new Error('User not found')
+    }
+
+    /**
+     *
+     * when delete a user
+     * we need to delete questions , answer , comments
+     *
+     */
+
+    const userQuestionsIds = await Question.find({ author: user._id }).distinct(
+      '_id'
+    )
+
+    await Question.deleteMany({ author: user._id })
+
+    /**
+     * @todo later delete User answers , comments and other things if left any
+     */
+
+    const deletedUser = await User.findByIdAndDelete(user._id)
+
+    return deletedUser
   } catch (error) {
     console.log(error)
   }
