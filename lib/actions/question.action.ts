6d7494ae4ac95aa1683901a6db1bfcd1e8f0c4f2
mdyousufhossain <yousafhossain3@gmail.com>
2023-12-20 12:38:34 +0600

    fixed eslint error

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -24,6 +24,7 @@ export async function createQuestion (params: CreateQuestionParams) {
   try {
     connectionToDatabase()
 
+    // eslint-disable-next-line no-unused-vars
     const { title, content, tags, author, path } = params
 
     const question = await Question.create({
