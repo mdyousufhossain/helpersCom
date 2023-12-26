2aaf34456127f1073914229f2166b2a7d7851952
mdyousufhossain <yousafhossain3@gmail.com>
2023-12-26 15:35:17 +0600

    edited the user card and improved the design

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link"
-
+import Link from 'next/link'
+import Image from 'next/image'
 interface Props {
   user: {
     _id: string
@@ -12,8 +12,19 @@ interface Props {
 
 const UserCard = ({ user }: Props) => {
   return (
-    <Link href={`/profile/${user.clerkId}`} >
-    
+    <Link
+      href={`/profile/${user.clerkId}`}
+      className='w-full  max-xs:min-w-full xs:w-[260px]'
+    >
+      <article className='background-light900_dark200 light-border flex w-full flex-col items-center justify-center rounded-2xl border p-8'>
+        <Image
+          src={user.picture}
+          alt={`${user.name} profile picture`}
+          width={100}
+          height={100}
+          className='rounded-full'
+        />
+      </article>
     </Link>
   )
 }
