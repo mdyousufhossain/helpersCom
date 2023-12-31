e54cb800986deeeda479fb9e4f178b05787491b6
mdyousufhossain <yousafhossain3@gmail.com>
2023-12-31 12:27:15 +0600

    Questions answer page is almost completed now we have to work on details and page content

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -1,6 +1,9 @@
 import { getQuestionsById } from '@/lib/actions/question.action'
 import Link from 'next/link'
 import Image from 'next/image'
+import Metric from '@/components/shared/Metric'
+import { formatNumber, getTimestamp } from '@/lib/utils'
+
 const Page = async ({ params, searchParams }) => {
   const result = await getQuestionsById({ questionId: params.id })
 
@@ -8,19 +11,53 @@ const Page = async ({ params, searchParams }) => {
     <>
       <div className='flex-start w-full flex-col'>
         <div className='flex w-full flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2'>
-          <Link href={`/profile/${result.author.clerkId}`}>
+          <Link href={`/profile/${result.author.clerkId}`}
+          className='flex items-end justify-start gap-1'
+          >
             <Image
               src={result.author.picture}
               width={22}
               height={22}
               alt={`${result.author.name}s profile picture`}
+              className='rounded-full'
             />
             <p className='paragraph-semibold text-dark300_light700'>
               {result.author.name}
             </p>
           </Link>
+          <div className='flex justify-end'>
+            VOTING
+          </div>
         </div>
+        <h2 className='h2-semibold text-dark200_light900 mt-3.5 w-full text-left'> {result.title}</h2>
       </div>
+
+      <div className='mb-8 mt-5 flex flex-wrap gap-4'>
+      <Metric
+          imgUrl='/assets/icons/clock.svg'
+          value={`${getTimestamp(result.createdAt)}`}
+          alt={'Clock Icons'}
+          title='Asked'
+          textStyles='small-medium text-dark400_light800'
+        />
+        <Metric
+          imgUrl='/assets/icons/message.svg'
+          value={formatNumber(result.answers.length)}
+          alt={'message'}
+          title='answers'
+          textStyles='small-medium text-dark400_light800'
+        />
+        <Metric
+          imgUrl='/assets/icons/eye.svg'
+          value={formatNumber(result.views)}
+          alt={'eye'}
+          title='views'
+          textStyles='small-medium text-dark400_light800'
+        />
+
+      </div>
+
+      {/* parsedContent */}
     </>
   )
 }
