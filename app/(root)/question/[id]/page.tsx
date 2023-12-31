38c7fffaac583a7b624b9dc2b6f3e00076c66fff
mdyousufhossain <yousafhossain3@gmail.com>
2023-12-31 11:54:07 +0600

    answerpage inisilaizing the author of the questions

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -1,7 +1,28 @@
-import Question from '@/database/question.model'
-
+import { getQuestionsById } from '@/lib/actions/question.action'
+import Link from 'next/link'
+import Image from 'next/image'
 const Page = async ({ params, searchParams }) => {
-  return <div>Page</div>
+  const result = await getQuestionsById({ questionId: params.id })
+
+  return (
+    <>
+      <div className='flex-start w-full flex-col'>
+        <div className='flex w-full flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2'>
+          <Link href={`/profile/${result.author.clerkId}`}>
+            <Image
+              src={result.author.picture}
+              width={22}
+              height={22}
+              alt={`${result.author.name}s profile picture`}
+            />
+            <p className='paragraph-semibold text-dark300_light700'>
+              {result.author.name}
+            </p>
+          </Link>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Page
