10e749b41926a39e06e8a9193f139b692d61e160
mdyousufhossain <yousafhossain3@gmail.com>
2024-01-02 23:26:35 +0600

    bug fixed

--- lib/validation.ts
+++ lib/validation.ts
@@ -6,7 +6,7 @@ export const QuestionsSchema = z.object({
     .min(5, 'Title must be more than 5 character')
     .max(130, 'title is too long'),
   explanation: z.string().min(50, 'Dont be shy explain a bit more'),
-  tags: z.array(z.string().min(1).max(15)).min(1).max(3),
+  tags: z.array(z.string().min(1).max(15)).min(1).max(3)
 })
 
 // new start of the next chapter aka backend
