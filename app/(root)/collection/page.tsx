83f037e1e0553af1759e4e9a66f42fb31de4c814
mdyousufhossain <yousafhossain3@gmail.com>
2024-01-13 22:35:26 +0600

    made few changes and added user acitons of getting saved content in the save page of separeted user

---
+++ app/(root)/collection/page.tsx
@@ -0,0 +1,63 @@
+import LocalSearch from '@/components/shared/search/LocalSearch'
+import Filter from '@/components/shared/Filter'
+import { QuestionFilters } from '@/constants/filters'
+import NoResult from '@/components/shared/NoResult'
+import QuestionsCard from '@/components/cards/QuestionsCard'
+import { formatNumber } from '@/lib/utils'
+import { getSavedQuestions } from '@/lib/actions/user.action'
+import { auth } from '@clerk/nextjs'
+// bal
+export default async function Home () {
+  
+  const result = await getSavedQuestions({
+
+  })
+
+  return (
+    <>
+        <h1 className='h1-bold text-dark100_light900'>Saved Question</h1>
+
+      <div className='mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center'>
+        <LocalSearch
+          route='/'
+          iconPosition='left'
+          imgSrc='/assets/icons/search.svg'
+          placeholder='Search for questions'
+          otherclasses='flex-1'
+        />
+        <Filter
+          filters={QuestionFilters}
+          otherclasses='min-h-[56px] sm:min-w-[170px]'
+        />
+      </div>
+
+      <div className='mt-10 flex w-full flex-col gap-6'>
+        {/* looping through question */}
+        {result.questions.length > 0
+          ? (
+              result.questions.map((question : any) => (
+            <QuestionsCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={formatNumber(question.upvotes)}
+              views={formatNumber(question.views)}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+              ))
+            )
+          : (
+          <NoResult
+            title=' No result founded or something unexpected occured 🚀'
+            description='Save Question from home page to see content'
+            link='/'
+            linkTitle='saved content'
+          />
+            )}
+      </div>
+    </>
+  )
+}
