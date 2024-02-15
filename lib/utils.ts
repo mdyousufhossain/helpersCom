adb398e8c0f08598c66b8328cff99b04aa52aa25
mdyousufhossain <yousafhossain3@gmail.com>
2024-02-15 13:45:13 +0600

    local search functionality

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from 'clsx'
+import qs from 'query-string'
 import { twMerge } from 'tailwind-merge'
 
 export function cn (...inputs: ClassValue[]) {
@@ -71,4 +72,23 @@ export const getJoinedDate = (date: Date): string => {
   const joinedDate = `${month} ${year}`
 
   return joinedDate
+}
+
+interface UrlformUrlQuery {
+  params : string
+  key : string
+  value: string | null
+}
+
+export const formUrlQuery = ({ params, key, value }: UrlformUrlQuery) => {
+  const currentUrl = qs.parse(params)
+
+  currentUrl[key] = value
+
+  return qs.stringifyUrl({
+    url: window.location.pathname,
+    query: currentUrl
+  },
+  { skipNull: true }
+  )
 }
