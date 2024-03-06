024211313ca47abe41ba0a27672d5404e5217751
mdyousufhossain <yousafhossain3@gmail.com>
2024-03-06 23:36:59 +0600

    some updated added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -75,13 +75,26 @@ const config: Config = {
         from: { height: 'var(--radix-accordion-content-height)' },
         // @ts-ignore
         to: { height: 0 }
+      },
+
+      spin: {
+        from: {
+          transform: 'rotate(0deg)'
+        },
+        to: {
+          transform: 'rotate(360deg);'
+        }
+      },
+      animation: {
+        // @ts-ignore
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        // @ts-ignore
+        'accordion-up': 'accordion-up 0.2s ease-out',
+        // @ts-ignore
+        spin: 'spin 3s linear infinite'
       }
     },
-    animation: {
-      'accordion-down': 'accordion-down 0.2s ease-out',
-      'accordion-up': 'accordion-up 0.2s ease-out'
-    }
-  },
-  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')]
+    plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')]
+  }
 }
 export default config
