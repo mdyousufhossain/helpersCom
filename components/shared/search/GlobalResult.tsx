2e89ec1b26c21d8e39fad69ec7c32beaf79bf58a
mdyousufhossain <yousafhossain3@gmail.com>
2024-03-07 12:01:20 +0600

    global search

--- components/shared/search/GlobalResult.tsx
+++ components/shared/search/GlobalResult.tsx
@@ -6,13 +6,12 @@ import { useSearchParams } from 'next/navigation'
 import Link from 'next/link'
 import Image from 'next/image'
 import GlobalFilter from './GlobalFilter'
+import { globalSearch } from '@/lib/actions/general.action'
 
 const GlobalResult = () => {
   const searchParams = useSearchParams()
   const [isLoading, setIsLoading] = useState(true)
-  const [result, setResult] = useState([
-    { type: 'bal', title: 'bal' }
-  ])
+  const [result, setResult] = useState([{ type: 'bal', title: 'bal' }])
 
   const global = searchParams.get('global')
   const type = searchParams.get('type')
@@ -24,6 +23,9 @@ const GlobalResult = () => {
 
       try {
         // everything every where all at once
+        const res = await globalSearch({ query: global, type })
+        // @ts-ignore
+        setResult(JSON.parse(res))
       } catch (error) {
         console.log(error)
         throw error
@@ -31,10 +33,27 @@ const GlobalResult = () => {
         setIsLoading(false)
       }
     }
+    if (global) {
+      fetchResult()
+    }
   }, [global, type])
 
   const renderLink = (type: string, id: string) => {
-    return ''
+    switch (type) {
+      case 'question':
+        return `/question/${id}`
+      case 'blog':
+        return `/blog/${id}`
+      case 'answer':
+        return `/question/${id}`
+      case 'tag':
+        return `/tags/${id}`
+      case 'user':
+        return `/profile/${id}`
+
+      default:
+        return '/'
+    }
   }
   return (
     <div className='absolute top-full z-10 mt-3 w-full bg-light-800 drop-shadow-sm dark:bg-dark-400'>
@@ -42,7 +61,7 @@ const GlobalResult = () => {
       {/* golbalfilter */}
       <GlobalFilter />
       <div className='space-y-5'>
-        <p className='text-dark400_light900 paragraph-semibold px-5'>
+        <p className='text-dark400_light900 paragraph-semibold mt-2 px-5'>
           Top Match
         </p>
         {isLoading
@@ -58,31 +77,37 @@ const GlobalResult = () => {
           <div className='flex flex-col gap-2'>
             {' '}
             {result.length > 0
-              ? result.map((item: any, index: number) => (
-                  <Link
-                   // href={() => renderLink('', 'id')}
-                   href={'/'}
-                    key={item.type + item.id + index}
-                    className='flex w-full cursor-pointer items-start gap-3 px-5 py-2.5 hover:bg-light-700/50 dark:bg-dark-500/50'
-                  >
-                    <Image
-                     src='/assets/icons/tag/svg'
-                     alt='tags'
-                     width={18}
-                     height={18}
-                     className='invert-colors mt-1 object-contain'
-                     />
-                     <div className='flex flex-col'>
-                          <p className='body-medium text-dark200_light800 line-clamp-1'>{item.title}</p>
-                          <p className='text-light400_light500 small-medium mt-1 font-bold capitalize'>{item.type}</p>
-                     </div>
-
-                  </Link>
-              ))
+              ? (
+                  result.map((item: any, index: number) => (
+                <Link
+                  href={renderLink(item.type, item.id)}
+                  key={item.type + item.id + index}
+                  className='flex w-full cursor-pointer items-start gap-3 px-5 py-2.5 hover:bg-light-700/50 dark:bg-dark-500/50'
+                >
+                  <Image
+                    src='/assets/icons/tag.svg'
+                    alt='tags'
+                    width={18}
+                    height={18}
+                    className='invert-colors mt-1 object-contain'
+                  />
+                  <div className='flex flex-col'>
+                    <p className='body-medium text-dark200_light800 line-clamp-1'>
+                      {item.title}
+                    </p>
+                    <p className='text-light400_light500 small-medium mt-1 font-bold capitalize'>
+                      {item.type}
+                    </p>
+                  </div>
+                </Link>
+                  ))
+                )
               : (
-                <div className='flex-center flex-col px-5'>
-                    <p className='text-dark200_light800 body-regular px-5 py-2.5'>Ooops , no result found</p>
-                </div>
+              <div className='flex-center flex-col px-5'>
+                <p className='text-dark200_light800 body-regular px-5 py-2.5'>
+                  Ooops , no result found
+                </p>
+              </div>
                 )}
           </div>
             )}
